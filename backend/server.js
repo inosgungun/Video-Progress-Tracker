2ebe1c8dcedc0af2e0946fdb8288ac4f5540c753
Gungun Soni <gungunbps2018@gmail.com>
2025-05-23 22:31:53 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+// backend/server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('MongoDB connected');
+  app.listen(5000, () => console.log('Server running on port 5000'));
+})
+.catch((err) => console.error(err));
